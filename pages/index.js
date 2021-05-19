7599df9862eb14598cec87fe7294c8c0492b9f5e
Ollie <3martynov@gmail.com>
2021-05-18 21:17:55 -0400

    added btcpay button

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,16 @@ export default function Home() {
         <h1 className={styles.title}>
           Hello world!
         </h1>
+        <div>
+        <form method="POST"  action="http://umbrel.local:3003/api/v1/invoices" class="btcpay-form btcpay-form--block">
+          <input type="hidden" name="storeId" value="54sgJMXR9PJ3xiY5V8jG7z1oWLRRyQe4rJEMujo75FUE" />
+          <input type="hidden" name="browserRedirect" value="https://btcpay.olliecodes.com?invoicePaid=true" />
+          <input type="hidden" name="checkoutQueryString" value="invoicePaid=true" ></input>
+          <input type="hidden" name="price" value="0.001" ></input>
+          <input type="hidden" name="currency" value="USD" ></input>
+          <input type="image" class="submit" name="submit" src="http://umbrel.local:3003/img/paybutton/pay.svg" style={{width: '120px'}}alt="Pay with BtcPay, Self-Hosted Bitcoin Payment Processor"></input>
+        </form>
+        </div>
       </main>
     </div>
   )
