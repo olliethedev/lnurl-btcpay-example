1a292cb658da8c3e6d26ed47303b4672a139b919
Ollie <3martynov@gmail.com>
2021-05-20 10:06:20 -0400

    adding session

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSession } from 'next-session';
 
-export default (req, res) => {
+const options = {}; //todo
+
+function handler (req, res) {
   console.log(req.body);
+  req.session.views = req.session.views ? req.session.views + 1 : 1;
   res.status(200).json({ name: 'John Doe' })
 }
+
+export default withSession(handler, options);
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { applySession } from 'next-session';
+
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const options = {}; //todo
+
+export default function Home({ views }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,20 +16,33 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         <h1 className={styles.title}>
           Hello world!
         </h1>
         <div>
+        <p>Session views: {views}</p>
         <form method="POST"  action="http://umbrel.local:3003/api/v1/invoices" class="btcpay-form btcpay-form--block">
           <input type="hidden" name="storeId" value="54sgJMXR9PJ3xiY5V8jG7z1oWLRRyQe4rJEMujo75FUE" />
+          <input type="hidden" name="orderId" value="abc123" />
           <input type="hidden" name="browserRedirect" value="https://btcpay.olliecodes.com?invoicePaid=true" />
           <input type="hidden" name="checkoutQueryString" value="invoicePaid=true" ></input>
           <input type="hidden" name="price" value="0.001" ></input>
           <input type="hidden" name="currency" value="USD" ></input>
-          <input type="image" class="submit" name="submit" src="http://umbrel.local:3003/img/paybutton/pay.svg" style={{width: '120px'}}alt="Pay with BtcPay, Self-Hosted Bitcoin Payment Processor"></input>
+          <input type="image" className="submit" name="submit" src="http://umbrel.local:3003/img/paybutton/pay.svg" style={{width: '120px'}}alt="Pay with BtcPay, Self-Hosted Bitcoin Payment Processor"></input>
         </form>
         </div>
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps({ req, res }) {
+  await applySession(req, res, options);
+  req.session.views = req.session.views ? req.session.views + 1 : 1;
+  return {
+    props: {
+      views: req.session.views
+    }
+  }
 }
