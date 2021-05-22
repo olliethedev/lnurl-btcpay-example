49d296219765688d1264a208d60c711cf1c83979
Ollie <3martynov@gmail.com>
2021-05-22 15:57:45 -0400

    debug session again

--- middleware/sessionMiddleware.js
+++ middleware/sessionMiddleware.js
@@ -2,6 +2,9 @@ import session from 'express-session';
 import connectMongo from 'connect-mongo';
 
 export default function sessionMiddleware(req, res, next) {
+    if(req.dbClient){
+        console.log("has dbclient")
+    }
   const mongoStore = new (connectMongo(session))({
     client: req.dbClient,
     stringify: false,
