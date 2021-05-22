ba571a21c6656f5fd7a192487fce796e7b4ff4e7
Ollie <3martynov@gmail.com>
2021-05-22 15:47:09 -0400

    followup

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import styles from '../styles/Home.module.css'
-// import appMiddleware from '../middleware/appMiddleware';
+import appMiddleware from '../middleware/appMiddleware';
 
 export default function Home({ views }) {
   return (
